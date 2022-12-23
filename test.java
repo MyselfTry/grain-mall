bbc29b857a545b6ac1391068b9a8b3883903b9d3
yangkaixuan <kaixuan.yang@wbstar.com>
2022-12-23 10:52:26 +0800

    test-m

--- test.java
+++ test.java
@@ -12,8 +12,8 @@ public class test {
     private void idea2(){
         System.out.println("2");
     }
-    private void a(int i){
-        System.out.println("a");
+    private void m(){
+        System.out.println("m");
     }
 
 }
