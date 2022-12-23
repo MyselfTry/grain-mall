ee46ee38e113a1b157168f8d0bd311ec849d3e91
yangkaixuan <kaixuan.yang@wbstar.com>
2022-12-23 09:37:46 +0800

    测试

---
+++ test.java
@@ -0,0 +1,12 @@
+/**
+*@ClassName test
+*@Description 
+*@Author coolSeason
+*@Date 2022/12/23 9:33
+*@Version 1.0
+*/public class test {
+
+    public static void main(String[] args){
+        System.out.println("master");
+    }
+}
